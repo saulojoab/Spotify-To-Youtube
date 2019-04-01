704aaecee7bdafedf4fd4f962bac2b50b7313a05
Saulo <saulojoabps@gmail.com>
2019-04-01 03:30:02 -0300

    fixing errors on getting artists from songs

--- spotifyToYoutube.py
+++ spotifyToYoutube.py
@@ -11,5 +11,19 @@ spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 results = spotify.user_playlist_tracks(user="",playlist_id="https://open.spotify.com/user/loudcodes_/playlist/4fIylfM1cQuMLr7tVQpSYn?si=909vsBwrSv6IH2o3n63SIQ")
 trackList = [];
+
 for i in results["tracks"]["items"]:
-    print(i["track"]["name"] + " - " + i["track"]["album"]["artists"][0]["name"])
+    # In case there's only one artist.
+    if (i["track"]["artists"].__len__() == 1):
+        trackList.append(i["track"]["name"] + " - " + i["track"]["artists"][0]["name"])
+    # In case there's more than one artist.
+    else:
+        nameString = "";
+        for index, b in enumerate(i["track"]["artists"]):
+            nameString += (b["name"]);
+            if (i["track"]["artists"].__len__() - 1 != index):
+                nameString += ", ";
+        trackList.append(i["track"]["name"] + " - " + nameString);
+
+for i in trackList:
+    print(i);
